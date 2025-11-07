1275188fdbadc202b9bd22989932f6ce54033e8b
Mateus Fernandes Pereira <mateus161004@gmail.com>
2025-11-07 20:02:48 -0300

    feature: domain layer product

--- src/main/java/com/pereira/catalog_service/domain/Product.java
+++ src/main/java/com/pereira/catalog_service/domain/Product.java
@@ -2,8 +2,11 @@ package com.pereira.catalog_service.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "products")
@@ -25,9 +28,29 @@ public class Product {
     private String description;
 
     @Column(nullable = false, precision = 10, scale = 2)
-    private BigInteger price;
+    private BigDecimal price;
 
     @Column(nullable = false, length = 100)
     private String category;
 
+    @Column(name = "image_url", nullable = false, length = 500)
+    private String imageUrl;
+
+    @Column(nullable = false)
+    private Boolean avaliable;
+
+    @Column(name = "stockQuantity", nullable = false)
+    private Integer stockQuantity;
+
+    @CreationTimestamp
+    @Column(name = "created_at",nullable = false)
+    private LocalDateTime createdAt;
+
+    @CreationTimestamp
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+
+    @CreationTimestamp
+    @Column(name = "deleted_at", nullable = false)
+    private LocalDateTime deletedAt;
 }
