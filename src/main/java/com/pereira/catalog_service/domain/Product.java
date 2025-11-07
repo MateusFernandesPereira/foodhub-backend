e7d3872a100d7f45adefcf0a20635f2a60fb5694
Mateus Fernandes Pereira <mateus161004@gmail.com>
2025-11-07 20:29:45 -0300

    feature: soft_delete querys

--- src/main/java/com/pereira/catalog_service/domain/Product.java
+++ src/main/java/com/pereira/catalog_service/domain/Product.java
@@ -3,13 +3,17 @@ package com.pereira.catalog_service.domain;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.annotations.Where;
 
 import java.math.BigDecimal;
-import java.math.BigInteger;
 import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "products")
+@SQLDelete(sql = "UPDATE products SET deleted_at = NOW() WHERE id = ?")
+@Where(clause = "deleted_at IS NULL")
 @Getter
 @Setter
 @NoArgsConstructor
@@ -19,7 +23,7 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(nullable = false, length = 255)
     private String name;
@@ -33,24 +37,24 @@ public class Product {
     @Column(nullable = false, length = 100)
     private String category;
 
-    @Column(name = "image_url", nullable = false, length = 500)
+    @Column(name = "image_url", length = 500)
     private String imageUrl;
 
     @Column(nullable = false)
-    private Boolean avaliable;
+    private Boolean available;
 
-    @Column(name = "stockQuantity", nullable = false)
+    @Column(name = "stock_quantity", nullable = false)
     private Integer stockQuantity;
 
     @CreationTimestamp
-    @Column(name = "created_at",nullable = false)
+    @Column(name = "created_at",nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
-    @CreationTimestamp
+    @UpdateTimestamp
     @Column(name = "updated_at", nullable = false)
     private LocalDateTime updatedAt;
 
-    @CreationTimestamp
-    @Column(name = "deleted_at", nullable = false)
+    //@CreationTimestamp aqui eu nao deveria usar um DeleteTimestamp, tem algo desse sentido no spring
+    @Column(name = "deleted_at")
     private LocalDateTime deletedAt;
 }
